8c13a9d6ccbc4d3fc612d12b5f2da4488c03fef8
Mathias-93 <iwbsc2@gmail.com>
2025-02-10 12:55:44 +0100

    connecting db and setting up backend APIs

--- server/db.js
+++ server/db.js
@@ -1,11 +1,12 @@
-const Pool = require("pg").Pool;
+require("dotenv").config();
+const { Pool } = require("pg");
 
 const pool = new Pool({
   user: process.env.DB_USER,
-  password: process.env.DB_PW,
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  password: process.env.DB_PW,
   database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 5432,
 });
 
 module.exports = pool;
--- server/server.js
+++ server/server.js
@@ -2,9 +2,20 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const PORT = 1337;
+const pool = require("./db");
 
 app.use(cors());
-app.use(express.json);
+app.use(express.json());
+
+app.get("/exercises", async (req, res) => {
+  try {
+    const allExercises = await pool.query("SELECT * FROM exercises");
+    res.status(200).json(allExercises.rows);
+  } catch (err) {
+    console.log(err.message);
+    res.status(500).json({ message: err.message });
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
