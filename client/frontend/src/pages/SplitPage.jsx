c49b55e21debb31abb91e593aabca1163d8d5ae6
Mathias-93 <iwbsc2@gmail.com>
2025-04-08 17:29:35 +0200

    started configuring front end to display workouts and exercises of the selected split

--- client/frontend/src/pages/SplitPage.jsx
+++ client/frontend/src/pages/SplitPage.jsx
@@ -7,7 +7,7 @@ export default function SplitPage() {
   const { userSplit, setUserSplit, isLoading, fetchUserSplit, setIsLoading } =
     useContext(GlobalContext);
   const [localUserSplit, setLocalUserSplit] = useState(null);
-  const [workouts, setWorkouts] = useState(null);
+  const [workouts, setWorkouts] = useState([]);
 
   const fetchWorkouts = async () => {
     try {
@@ -30,6 +30,10 @@ export default function SplitPage() {
     } finally {
       setIsLoading(false);
     }
+  };
+
+  const handleChangeExercise = () => {
+    console.log("Change!");
   };
 
   useEffect(() => {
@@ -61,45 +65,47 @@ export default function SplitPage() {
   }
 
   return (
-    <div className="w-full min-h-screen p-6 bg-gray-100 dark:bg-gray-900 pt-[250px] flex justify-center">
-      <div className="w-full min-w-[500px] max-w-3xl bg-white dark:bg-gray-800 p-8 rounded-xl shadow-lg border border-gray-200 dark:border-gray-700">
-        <h1 className="text-3xl font-bold text-center text-gray-800 dark:text-gray-200">
-          {localUserSplit?.name}
-        </h1>
-
-        <div className="mb-5">
-          <label className="block text-gray-700 dark:text-gray-300 font-medium mb-1">
-            Edit split
-          </label>
-          <input
-            type="text"
-            placeholder=""
-            className="w-full p-3 border border-gray-300 dark:border-gray-600 rounded-lg bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-white focus:ring-2 focus:ring-blue-500 focus:outline-none"
-          />
-        </div>
-
-        <div className="mb-6">
-          <h2 className="text-lg font-semibold text-gray-900 dark:text-gray-100 mb-2">
-            Exercise
-          </h2>
-          {/* {currentSplit.map(() => {
-            <div className="flex items-center space-x-3">
-              <input
-                type="text"
-                placeholder=""
-                className="flex-1 mt-2 p-3 border border-gray-300 dark:border-gray-600 rounded-lg bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-white focus:ring-2 focus:ring-blue-500 focus:outline-none"
-              />
-            </div>;
-          })} */}
-        </div>
-        <div className="flex justify-center mt-6">
-          <button
-            type="submit"
-            className="w-1/2 bg-green-500 text-white py-3 rounded-lg ml-2 hover:bg-green-600 transition"
+    // Whole thing
+    <div className="w-full min-h-screen p-6 bg-gray-100 dark:bg-gray-900 pt-[250px] flex flex-col gap-10 mt-10 justify-center items-center">
+      {/* Form */}
+      {workouts.map((workout) => {
+        return (
+          <div
+            key={workout.workout_id}
+            className="w-full min-w-[500px] max-w-3xl bg-white dark:bg-gray-800 p-8 rounded-xl shadow-lg border border-gray-200 dark:border-gray-700"
           >
-            Save Split
-          </button>
-        </div>
+            <h1 className="text-3xl font-bold text-center text-gray-800 dark:text-gray-200">
+              {workout?.name}
+            </h1>
+            {workout.exercises.map((exercise, index) => {
+              return (
+                <div key={index}>
+                  <div className="mb-6">
+                    <h2 className="text-lg font-semibold text-gray-900 dark:text-gray-100 mb-2">
+                      Exercise {index + 1}
+                    </h2>
+                    <div className="flex items-center space-x-3">
+                      <input
+                        type="text"
+                        value={exercise.name}
+                        onChange={() => handleChangeExercise()}
+                        className="flex-1 mt-2 p-3 border border-gray-300 dark:border-gray-600 rounded-lg bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-white focus:ring-2 focus:ring-blue-500 focus:outline-none"
+                      />
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
+      <div className="flex justify-center mt-6">
+        <button
+          type="submit"
+          className="w-1/2 bg-green-500 text-white py-3 rounded-lg ml-2 hover:bg-green-600 transition"
+        >
+          Save Split
+        </button>
       </div>
     </div>
   );
