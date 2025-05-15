0b53423500508feaed4230b3d5cfef58312e9766
Mathias-93 <iwbsc2@gmail.com>
2025-05-15 19:57:02 +0200

    implemented search fields and filter for PR's based on exercise and type of PR

--- client/frontend/src/pages/PrsAndGoals.jsx
+++ client/frontend/src/pages/PrsAndGoals.jsx
@@ -3,11 +3,46 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { GlobalContext } from "../Context";
 import Spinner from "../components/Spinner";
+import SuggestionDropdown from "../components/SuggestionDropdown";
+import { useClickOutsideAndEscape } from "../hooks/useClickOutsideAndEscape";
 
 export default function PrsAndGoals() {
   const { setIsLoading, showSpinner, exercises } = useContext(GlobalContext);
   const [temporary, setTemporary] = useState(null);
   const [prsData, setPrsData] = useState(null);
+  const [activeDropdown, setActiveDropdown] = useState(null);
+  const [filteredExercises, setFilteredExercises] = useState([]);
+  const [prTypes, setPrTypes] = useState(["weight", "volume"]);
+  const dropdownref = useClickOutsideAndEscape(() => {
+    setActiveDropdown(null);
+    setFilteredExercises([]);
+  });
+  const [selectedExercises, setSelectedExercises] = useState({});
+
+  const handleExercisesFilter = (query, type) => {
+    const lowerQuery = query.toLowerCase();
+
+    if (lowerQuery.length > 1) {
+      const filteredData =
+        exercises?.filter((exercise) =>
+          exercise.name.toLowerCase().includes(lowerQuery)
+        ) || [];
+
+      setFilteredExercises(filteredData);
+      setActiveDropdown(type);
+    } else {
+      setActiveDropdown(null);
+    }
+  };
+
+  const handleClickDropdown = (exerciseName, type) => {
+    setSelectedExercises((prev) => ({
+      ...prev,
+      [type]: exerciseName,
+    }));
+    setActiveDropdown(null);
+    setFilteredExercises([]);
+  };
 
   const fetchUserPrs = async () => {
     try {
@@ -51,24 +86,56 @@ export default function PrsAndGoals() {
     }
   }, [exercises, temporary]);
 
+  /* console.log(prsData); */
+
   if (showSpinner) {
     <Spinner />;
   }
 
   return (
     <div className="w-full min-h-screen p-6 bg-gray-100 dark:bg-gray-900 pt-[200px] flex flex-col gap-10 mt-10 items-center">
-      <h1>PrsAndGoals</h1>
-      {prsData
-        ? prsData.map((pr, index) => {
-            return (
-              <div key={index}>
-                <p>
-                  {pr.pr_type} PR for {pr.exercise_name}: {pr.value}
-                </p>
-              </div>
-            );
-          })
-        : null}
+      <h1 className="dark:text-white">PRs and Goals</h1>
+      <div className="flex gap-10">
+        {prTypes.map((type, index) => {
+          return (
+            <div key={index} className="relative">
+              <h2 className="dark:text-white">{type}</h2>
+              <input
+                type="text"
+                value={selectedExercises[type] || ""}
+                onChange={(e) => {
+                  handleExercisesFilter(e.target.value, type);
+                  setSelectedExercises((prev) => ({
+                    ...prev,
+                    [type]: e.target.value,
+                  }));
+                }}
+                placeholder={"Search for an exercise"}
+                className="flex-1 mt-2 p-3 border border-gray-300 dark:border-gray-600 rounded-lg bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-white focus:ring-2 focus:ring-blue-500 focus:outline-none"
+              />
+              ;
+              {activeDropdown === type && filteredExercises?.length > 0 && (
+                <SuggestionDropdown
+                  ref={dropdownref}
+                  data={filteredExercises}
+                  handleClickDropdown={(exerciseName) =>
+                    handleClickDropdown(exerciseName, type)
+                  }
+                />
+              )}
+              {prsData?.map((pr) =>
+                pr.pr_type === type &&
+                pr.exercise_name === selectedExercises[type] ? (
+                  <p className="dark:text-white">
+                    Max {type === "weight" ? "weight" : "volume"} lifted for{" "}
+                    {pr.exercise_name}: {pr.value}
+                  </p>
+                ) : null
+              )}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
