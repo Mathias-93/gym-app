3b2484fffe1a576409d8769bda5d1f4b201b8430
Mathias-93 <iwbsc2@gmail.com>
2025-05-25 15:49:58 +0200

    added package for deployment of frontend

--- server/server.js
+++ server/server.js
@@ -11,17 +11,25 @@ import goalRoutes from "./routes/goalRoutes.js";
 import exerciseRoutes from "./routes/exerciseRoutes.js";
 import authenticateToken from "./middleware/authMiddleware.js";
 
-const PORT = 1337;
+const PORT = process.env.PORT || 1337;
 const app = express();
 
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"], // Allow frontend access
-    credentials: true, // Allows cookies (JWT) to be sent
+    origin: process.env.CORS_ORIGIN?.split(",") || [
+      "http://localhost:5173",
+      "http://127.0.0.1:5173",
+    ],
+    credentials: true,
   })
 );
 app.use(express.json());
+
+// Health check route
+app.get("/", (req, res) => {
+  res.send("API is running!");
+});
 
 // Any api requests that has to do with registration and login
 app.use("/auth", authRoutes);
@@ -40,27 +48,6 @@ app.use("/prs", authenticateToken, prRoutes);
 
 // Any api requests that has to do with goals
 app.use("/goals", authenticateToken, goalRoutes);
-
-app.get("/", async (req, res) => {
-  try {
-    // Loading front end
-    console.log("Hello");
-    res.status(200).json({ message: "Yo" });
-  } catch (err) {
-    console.log(err.message);
-    res.status(500).json({ message: err.message });
-  }
-});
-
-app.get("/users", authenticateToken, async (req, res) => {
-  try {
-    const allUsers = await pool.query("SELECT * FROM users");
-    res.status(200).json(allUsers.rows);
-  } catch (err) {
-    console.log(err.message);
-    res.status(500).json({ message: err.message });
-  }
-});
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
