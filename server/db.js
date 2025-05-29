6dcb9904393eeaacecc260300b8e900a9adacee1
Mathias-93 <iwbsc2@gmail.com>
2025-05-29 10:58:29 +0200

    testing

--- server/db.js
+++ server/db.js
@@ -2,11 +2,19 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // Supabase requires SSL
+  },
+});
+
+/* const pool = new Pool({
   connectionString: process.env.DATABASE_URL, // Use DATABASE_URL directly
   ssl:
     process.env.NODE_ENV === "production"
       ? { rejectUnauthorized: false }
       : false, // Allow SSL for production
-});
+
+}); */
 
 export default pool;
